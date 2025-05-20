63cbfe49afb8cf866dc94f170e427b06d9e4a34c
KESAM REDDY PRASHANTH REDDY <160251291+Kesamreddyprashanthreddy@users.noreply.github.com>
2025-05-20 08:09:14 +0530

    Time: 74 ms (19.47%) | Memory: 17.9 MB (11.54%) - LeetSync

--- 338-counting-bits/counting-bits.py
+++ 338-counting-bits/counting-bits.py
@@ -1,7 +1,18 @@
-class Solution:
-    def countBits(self, n: int) -> List[int]:
+class Solution(object):
+    def countBits(self, n):
+        """
+        :type n: int
+        :rtype: List[int]
+        """
+
         arr = []
         for i in range(n+1):
-            res = bin(i).count('1')
-            arr.append(res)
+            count = 0
+            num = i
+            while num > 0:
+                if num & 1:
+                    count += 1
+                num = num >> 1
+            arr.append(count)
         return arr
+        
