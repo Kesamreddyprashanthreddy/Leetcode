400cd74e73ce674de40a394a08e62db19dff28e7
KESAM REDDY PRASHANTH REDDY <160251291+Kesamreddyprashanthreddy@users.noreply.github.com>
2025-09-16 23:41:16 +0530

    Time: 0 ms (100.00%) | Memory: 18 MB (16.20%) - LeetSync

--- 118-pascals-triangle/pascals-triangle.py
+++ 118-pascals-triangle/pascals-triangle.py
@@ -1,22 +1,15 @@
 class Solution:
     def generate(self, numRows: int,memo={}) -> List[List[int]]:
-        memo = {}
-        def pascal(row, col):
-            if (row, col) in memo:
-                return memo[(row, col)]
-            
-            if col == 0 or col == row:
-                return 1
-            memo[(row, col)] = pascal(row-1, col-1) + pascal(row-1, col)
-            return memo[(row, col)]
-        
         triangle = []
+        
         for r in range(numRows):
-            row_vals = []
-            for c in range(r+1):
-                row_vals.append(pascal(r, c))
-            triangle.append(row_vals)
+            row = [1] * (r + 1)
+            for c in range(1, r):
+                row[c] = triangle[r-1][c-1] + triangle[r-1][c]
+            
+            triangle.append(row)
         
         return triangle
+
         
                 
