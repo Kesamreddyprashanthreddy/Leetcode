c1d09bb5b281227fe6c390c3141189ff1d5fc2fb
KESAM REDDY PRASHANTH REDDY <160251291+Kesamreddyprashanthreddy@users.noreply.github.com>
2025-10-11 23:57:41 +0530

    Time: 6 ms (31.48%) | Memory: 18.7 MB (42.62%) - LeetSync

---
+++ 2398-check-if-matrix-is-x-matrix/check-if-matrix-is-x-matrix.py
@@ -0,0 +1,12 @@
+class Solution:
+    def checkXMatrix(self, grid: List[List[int]]) -> bool:
+        m,n = len(grid),len(grid[0])
+        for i in range(m):
+            for j in range(n):
+                if i == j or i + j == n - 1:
+                    if grid[i][j] == 0:
+                        return False
+                else:
+                    if grid[i][j] != 0:
+                        return False          
+        return True  
