0c9634fd839155a9c91bba72d166ae900f9d4355
KESAM REDDY PRASHANTH REDDY <160251291+Kesamreddyprashanthreddy@users.noreply.github.com>
2025-10-31 23:12:35 +0530

    Time: 5 ms (8.84%) | Memory: 17.9 MB (24.82%) - LeetSync

---
+++ 1651-shuffle-string/shuffle-string.py
@@ -0,0 +1,8 @@
+class Solution:
+    def restoreString(self, s: str, indices: List[int]) -> str:      
+        list_s = list(s)
+        res = ""
+        for i in range(len(s)):
+            idx = indices.index(i)
+            res += s[idx]
+        return res
