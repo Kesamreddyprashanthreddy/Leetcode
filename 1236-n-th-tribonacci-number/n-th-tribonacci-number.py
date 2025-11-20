3d227c35f5206c02d77cf22735008f8af7aa6cf5
KESAM REDDY PRASHANTH REDDY <160251291+Kesamreddyprashanthreddy@users.noreply.github.com>
2025-11-21 00:37:39 +0530

    Time: 0 ms (100.00%) | Memory: 17.7 MB (49.01%) - LeetSync

--- 1236-n-th-tribonacci-number/n-th-tribonacci-number.py
+++ 1236-n-th-tribonacci-number/n-th-tribonacci-number.py
@@ -7,5 +7,5 @@ class Solution:
         dp = [0] * (n+1)
         dp[1],dp[2] = 1,1
         for i in range(3,n+1):
-            dp[i] = dp[i-1] + dp[i-2] + dp[i-3]
+           dp[i] = dp[i-1] + dp[i-2] + dp[i-3]
         return dp[n]
